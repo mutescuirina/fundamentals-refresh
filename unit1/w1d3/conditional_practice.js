4be3908130b987d446335dbe1e96ad4201147f5a
mutescuirina <irina.a@irinas-MBP.home>
2021-05-21 09:54:36 -0400

    conditional_practice completed

--- unit1/w1d3/conditional_practice.js
+++ unit1/w1d3/conditional_practice.js
@@ -43,5 +43,87 @@
 
 //Practice thruthiness and falsiness
 
-console.log(!!1);
+// console.log(!!1);//true//
+// console.log(!!0);//true//
+// console.log(!!-1);//true//
+// console.log(!![]);//true//
+// console.log(!!{});//true//
+// console.log(!!null);//false//
+// console.log(!!"");//false//
 
+//Equality operators//
+
+// console.log(1 == 1) //true//
+// console.log("beans" == "beans")//true//
+// console.log(5+5*3 == ((5+5)*3))//true//
+// console.log(9 !== false) //true//
+// console.log(NaN == NaN)//false//
+
+// let mmColor = 'blue'
+// if(mmColor ==='green') {
+//     console.log('Eiw! I am hurling this gross green M&M across the room.')
+// } else {
+//     console.log('Yum! M&Ms are so tasty!')
+// }
+
+
+// ## Comparison Operators//
+
+// console.log("A" > "a")//false//
+
+// "b" > "a"//false//
+
+// 12 > "12"//false//
+
+// 12 >= "12"//false//
+
+
+// `'a' > 'b'`; //false//
+
+// `'z' > 'abc'`
+// => true
+
+//Logical operators
+
+// let username = ''
+
+// let user = username || 'Bob Bobby Bob';
+// console.log(user);
+
+//### Boolean order of evaluation//
+
+//Order of evaluation matters! When you have a complex statement, be sure to remember the order things will be evaluated//
+
+// console.log(!false && true)//true//
+// console.log(false || true) //true//
+
+// const a = true;
+// const b = false;
+// console.log(a && a == b)//false//
+// console.log(!true || !false && !false)//true//
+// console.log(8 > 1 && true || false)//true//
+
+//## Modulo//
+//_Modulus as conditionals_//
+
+//The **modulo** operator (also referred to as modulus) is represented as a `%` in JavaScript. The Modulo returns the remainder of Euclidean division (Don't worry! It's not as scary as it sounds! Let's investigate together!)//
+
+// console.log(4 % 2)//0
+
+if (5 % 2 === 0) {
+    console.log('number is even')
+}
+
+if (5 % 2 !== 0) {
+    console.log('number is odd')
+} 
+
+if (49 % 7 === 0) {
+    console.log('number is divisible by 7')
+}
+
+if (5 % 2 == 0) {
+    console.log('Number is even');
+} else {
+    console.log('Number is odd')
+}
