f605b0a74dac3029a6dfebc458b16f9dfe7fea85
mutescuirina <irina.a@irinas-MBP.home>
2021-05-24 12:03:26 -0400

    wild wild life answered

--- unit1/w1d3/homework.js
+++ unit1/w1d3/homework.js
@@ -20,13 +20,35 @@
 
             if
             ((checkForThree === 0) && (checkForFive === 0)) {
-                console.log("FizzBuzz")
+                // console.log("FizzBuzz")
             } else if(checkForFive === 0) {
-                console.log('Buzz')
+                // console.log('Buzz')
             } else if(checkForThree === 0)  {
-                console.log("Fizz")
+                // console.log("Fizz")
             } else {
-                console.log(i)
+                // console.log(i)
             }
         }
 
+//#### Wild Wild Life
+//Use the following arrays to answer the questions below (name,species ,age, hometown):
+//You should be modifying the elements by accessing them. It is up to you which methods to use.
+
+const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
+const sharky = ["Sharky", "shark", 20, "Left Coast"]
+const plantee = ["Plantee", "plant",  5000 , "Mordor"]
+const porgee = ["Porgee", "Porg", 186, "Ahch-To"]
+const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down"]
+
+//1. Plantee just had her birthday; change Plantee's array to reflect her being a year older.
+plantee[2] = 5001;
+console.log(plantee);
+//2. Change Wolfy's hometown from "Yukon Territory" to "Gotham City".
+wolfy[3] = "Gotham City";
+console.log(wolfy);
+//3. Give D'Art a second hometown by adding "Hawkins"
+dart.unshift('Hawkins');
+console.log(dart);
+//4. Porgee decides that Wolfy can't be named "Wolfy" anymore. Remove "Wolfy" from the `wolfy` array and replace it with "Gameboy".
+wolfy[0] = "Gameboy";
+console.log(wolfy);
