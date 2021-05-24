e9f934e3bd687dccec55e0b281382c396e3901b6
mutescuirina <irina.a@irinas-MBP.home>
2021-05-24 11:00:56 -0400

    Get even answered

--- unit1/w1d3/homework.js
+++ unit1/w1d3/homework.js
@@ -1,7 +1,11 @@
 //JavaScript Reps
 
-//Write a for loop that will log the numbers 1 through 20.
+    //Write a for loop that will log the numbers 1 through 20.
 
-for (i = 0; i <= 10; i++){
-    console.log(i);
-}
+        for (i = 0; i <= 10; i++){
+            console.log(i);
+        }
+    //Write a for loop that will log only the even numb ers in 0 through 200. 
+        for (i = 0; i <=200; i+=2) {
+            console.log(i);
+        }
