1eb5dd7ab86748d2e1e0336a391433e5ca145b32
mutescuirina <irina.a@irinas-MBP.home>
2021-05-26 14:11:05 -0400

    functions.js completed

--- unit1/w1d4/functions.js
+++ unit1/w1d4/functions.js
@@ -4,81 +4,81 @@
 
 //## Defining a function
 
-    // const printBoo = () => {
-    //     console.log("======");
-    //     console.log('Boo!');
-    //     console.log('======');
-    // }
-    // //the code will not run until you invoke it!
-    // // use one line of code to run multiples line of code!
-    // printBoo();
+// const printBoo = () => {
+//     console.log("======");
+//     console.log('Boo!');
+//     console.log('======');
+// }
+// //the code will not run until you invoke it!
+// // use one line of code to run multiples line of code!
+// printBoo();
 
 //
 
 //### Exercise
-    // const printSum = () => {
-    //     console.log(10 + 10);
-    // }
-    // printSum();
+// const printSum = () => {
+//     console.log(10 + 10);
+// }
+// printSum();
 
-    // const printTriangle = () => {
-    //     for (i = 0; i < 5; i++) {
-    //         let num = "#"
-    //         let numIncrement = num += num;
-    //         let total = numIncrement + num;
-    //         console.log( to);
-    //     }
-    //     // console.log("#");
-    //     // console.log("##");
-    //     // console.log("###");
-    //     // console.log("####");
-    //     // console.log("#####");
-    // }
-    // printTriangle();
-    
-    // function makeLine(length) {
-    //     var line = "";
-    //     for (var i = 1; i <= length; i++) {
-    //       for (var j = 1; j <= i; j++) {
-    //         line += "*";
-      
-    //       }
-    //       line += "\n";
-    //     }
-    //     return line;
-    //   }
-    //   console.log(makeLine(5));
+// const printTriangle = () => {
+//     for (i = 0; i < 5; i++) {
+//         let num = "#"
+//         let numIncrement = num += num;
+//         let total = numIncrement + num;
+//         console.log( to);
+//     }
+//     // console.log("#");
+//     // console.log("##");
+//     // console.log("###");
+//     // console.log("####");
+//     // console.log("#####");
+// }
+// printTriangle();
 
-    // const checkInputLenght = (input) => {
-    //     if (input.length > 10) {
-    //         console.log('input length is greater than 10')
-    //     } else {
-    //         console.log('input lenght is not greater that 10')
-    //     }
+// function makeLine(length) {
+//     var line = "";
+//     for (var i = 1; i <= length; i++) {
+//       for (var j = 1; j <= i; j++) {
+//         line += "*";
 
-    // }
-    // checkInputLenght('kkkkkfffffff');
+//       }
+//       line += "\n";
+//     }
+//     return line;
+//   }
+//   console.log(makeLine(5));
 
-    // const sayName = (name) => {
-    //     console.log('Hello! My name is ' + name);
-    // }
-    // sayName('Irina');
+// const checkInputLenght = (input) => {
+//     if (input.length > 10) {
+//         console.log('input length is greater than 10')
+//     } else {
+//         console.log('input lenght is not greater that 10')
+//     }
 
-    // Argument vs Parameter
+// }
+// checkInputLenght('kkkkkfffffff');
 
-    //The argument is the input, the parameter is how the input is represented in the function.
-        // const func =(PARAMETER) => {
-        //     //some code 
-        // }
-        // func(ARGUMENT);
+// const sayName = (name) => {
+//     console.log('Hello! My name is ' + name);
+// }
+// sayName('Irina');
 
-    //A function can take any number of parameters.
+// Argument vs Parameter
 
-    // const calculateArea = (num1, num2) => {
-    //     console.log(num1 * num2)
-    // }
+//The argument is the input, the parameter is how the input is represented in the function.
+// const func =(PARAMETER) => {
+//     //some code 
+// }
+// func(ARGUMENT);
 
-    // calculateArea(4,4);
+//A function can take any number of parameters.
+
+// const calculateArea = (num1, num2) => {
+//     console.log(num1 * num2)
+// }
+
+// calculateArea(4,4);
 
 
 //Exercises
@@ -119,55 +119,96 @@
 //**Extra**
 
 //* Write a function `divideThreeNums` that takes **three** parameters and prints the third parameter divided by the result of the second parameter divided by the first.
-    
-        // const divideThreeNums = (pram1,pram2,pram3) => {
-        //     console.log(Math.floor(pram3 / (pram2 / pram1)));
-        // }
-        // divideThreeNums(10, 5, 2)
-        // divideThreeNums(30, 2, 9)
+
+// const divideThreeNums = (pram1,pram2,pram3) => {
+//     console.log(Math.floor(pram3 / (pram2 / pram1)));
+// }
+// divideThreeNums(10, 5, 2)
+// divideThreeNums(30, 2, 9)
 //
 
 //Functions can pass information back to whatever line of code invoked them.
-    // const ten = () => {
-    //     return 10;
-    // }
-    // console.log(8 + ten());
+// const ten = () => {
+//     return 10;
+// }
+// console.log(8 + ten());
 
 //A function is only defined if it has a return value.
 
-        // const calculateArea = (num1, num2) => {
-        //     return num1 * num2
-        // }
+// const calculateArea = (num1, num2) => {
+//     return num1 * num2
+// }
 
-        // console.log(calculateArea(2,10));
-        // console.log(calculateArea(calculateArea(2,3), calculateArea(9,4)));
-
-
-        //Write a function that will **return** true if a word is a Palindrome, or will **return** false if not.
-            //// first you need  write a function that to splits str into an array
-    //    const reverseString = (str) => {
-    //         let splitString = str.split("");
-    //         let reverseArray = splitString.reverse();
-    //         let joinArray = reverseArray.join("");
-    //         return joinArray;
-
-    //     }
-       
-   
-
-     const checkForPalindrome = (word) => {
-         let splitWord = word.split('');
-         let reverseArray = splitWord.reverse();
-         let joinArray = reverseArray.join('');
-        
-        if (word === joinArray) {
-            return "true"
-         } 
-         else {
-             return "false"
-         }
-     }
-     console.log(checkForPalindrome('mad'));
+// console.log(calculateArea(2,10));
+// console.log(calculateArea(calculateArea(2,3), calculateArea(9,4)));
 
 
+//Write a function that will **return** true if a word is a Palindrome, or will **return** false if not.
+//// first you need  write a function that to splits str into an array
+//    const reverseString = (str) => {
+//         let splitString = str.split("");
+//         let reverseArray = splitString.reverse();
+//         let joinArray = reverseArray.join("");
+//         return joinArray;
+
+//     }
+
+
+
+// const checkForPalindrome = (word) => {
+//     //splits str
+//     let splitWord = word.split('');
+//     //reverse the new created array
+//     let reverseArray = splitWord.reverse();
+//     //join the reverse aray into str
+//     let joinArray = reverseArray.join('');
+
+//     if (word === joinArray) {
+//         return "true"
+//     }
+//     else {
+//         return "false"
+//     }
+// }
+// console.log(checkForPalindrome('mad'));
+
+//### Exercises
+
+//* Write a function that takes two parameters (strings) and returns `true` (Boolean) if the two strings are identical, `false` if not.
+const checkforIdenticalStr = (str1, str2) => {
+    if (str1 === str2 ) {
+        return 'true';
+    } else {
+        return 'false';
+    }
+}
+console.log(checkforIdenticalStr('you','you'));
+
+//* Write a function that takes three parameters (numbers), sums them, converts the sum into a string and returns the string (eg. `'123'`)
+const compareThreeNums = (num1, num2, num3) => {
+    let sumNums = num1 + num2 + num3; 
+    let sumNumsString = sumNums.toString();
+
+    console.log(sumNumsString);
+    console.log(typeof sumNumsString)
+    return sumNumsString;
+    
+}
+compareThreeNums(2,4,6);
+
+// const compareThreeNums = (num1,num2,num3) => {
+//     let sumNums = num1 + num2 + num3;
+//     let sumNumsString = sumNums.toString();
+
+//     console.log(sumNumsString); 
+//     console.log(typeof sumNumsString); 
+//     return sumNumsString;
+// }
+
+// compareThreeNums(5,8,10);
+
+
+
+//* Use your google-fu to research converting a number into a string
+//* Invoke the function a couple of times with different arguments each time
 
