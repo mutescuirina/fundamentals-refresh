8e6f3e5a37870f8d24524c29abe1aec3bd91137f
mutescuirina <irina.a@irinas-MBP.home>
2021-05-28 11:54:43 -0400

    palindrome completed

--- unit1/w1d4/homework.js
+++ unit1/w1d4/homework.js
@@ -4,4 +4,25 @@
 //2.Within a function what is the difference between console.log and return. 
     //Console.log is a tool used to check that the code is doing what is intended to do where on the other side return executes the code. 
 
-//
+//3.Write a function `checkPalindrome` that accepts a single argument, a string. The function should return true (Boolean) if the string is a palindrome, false if it is not. Make sure your function will give the correct answer for words with **capital letters**.
+
+const checkPalindrome = (word) => {
+    // 1.you first have to split the word
+    let splitWord = word.split('');
+    // 2. then you have to reverse the new created array
+    let reverseArray = splitWord.reverse();
+    // 3. you have to join the letters again
+    let joinArray = reverseArray.join('').toUpperCase('');
+        // joinArray = joinArray.toUpperCase('');
+        word = word.toUpperCase('');
+    
+    // check if the word is equal to reverse version
+    if(word === joinArray) {
+        return true, " the word is a Palindrome"
+    } else {
+        return false, "the word is NOT a Palindrome "
+    }
+    
+}
+console.log(checkPalindrome('Radar'));
+console.log(checkPalindrome("Borscht"));
