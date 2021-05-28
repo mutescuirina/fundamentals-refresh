859570cf1010d6356139f5d7e0a9a37a15b41247
mutescuirina <irina.a@irinas-MBP.home>
2021-05-28 11:30:35 -0400

    verbal questions answered

---
+++ unit1/w1d4/homework.js
@@ -0,0 +1,7 @@
+//1. What is the difference between a parameter and an argument?
+    // A parameter is a set of rules that you can write with a function and an      argument is the data that is passed with the set of rules from the parameter. 
+
+//2.Within a function what is the difference between console.log and return. 
+    //Console.log is a tool used to check that the code is doing what is intended to do where on the other side return executes the code. 
+
+//
